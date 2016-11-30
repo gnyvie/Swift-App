5d6f1175ac25d6aa176322b371c65d6913bf5bc8
Güney Saritas <mail@guneysaritas.net>
2016-11-30 11:06:24 +0100

    Sum field, textfields

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -12,16 +12,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
-
-        tapCount = tapCount + 1
         
-        if tapCount >= 10{
-            theLabel.text = "10 MAL?!"
-        }
-        
+        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)" 
     }
     
     
